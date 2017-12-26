4a456cdb3d751216a3b033a4751f2e8196ca1225
TereshchenkoAnastasiya <anastasiya.tereshhenko.2015@gmail.com>
2017-12-26 10:23:40 +0600

    Matlab update

--- MatLAB/Tereshchenko/exp_plot.m
+++ MatLAB/Tereshchenko/exp_plot.m
@@ -5,18 +5,25 @@ A=1;
 
 t_1=16; 
 t_2=5; 
-
-
+k=9;
+l=5;
+M=16;
 
 %% Plot graph 
 x=-10:1:100; 
  y=exponential(x,A,t_1,t_2); 
- plot(x,y); 
-  grid on; 
-  title('График') 
-  xlabel('x') 
- ylabel('y')
+% plot(x,y); 
+%  grid on; 
+%  title('График') 
+%  xlabel('x') 
+%  ylabel('y')
 
- 
+ s=filtr(y,k,l,M);
+ plot(x, max(y)/ max(s) * s, x, y);
+ grid on; 
+ title('График') 
+ xlabel('x') 
+ ylabel('dlk')
+
 
 
